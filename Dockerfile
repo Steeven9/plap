5dfccf3812391d838dd464de34853d53a59e81a0
Stefano Taillefert <stefano.taille@gmail.com>
2025-03-21 02:54:57 +0100

    Add CI, localstorage, confetti

--- Dockerfile
+++ Dockerfile
@@ -5,38 +5,25 @@ FROM base AS deps
 WORKDIR /app
 COPY package.json yarn.lock ./
 
-RUN apk add --no-cache libc6-compat && \
-  yarn --frozen-lockfile
+RUN yarn --frozen-lockfile
 
 
 FROM base AS builder
 
 WORKDIR /app
-ENV NEXT_TELEMETRY_DISABLED=1
-
-COPY --from=deps /app/node_modules ./node_modules
-COPY . .
-
-RUN yarn build
-
-
-FROM base AS runner
-
-WORKDIR /app
-ENV NODE_ENV=production
-ENV NEXT_TELEMETRY_DISABLED=1
-EXPOSE 3000
-ENV PORT=3000
-ENV HOSTNAME="0.0.0.0"
 
 RUN addgroup --system --gid 1001 nodejs && \
   adduser --system --uid 1001 nextjs && \
   mkdir .next && \
   chown nextjs:nodejs .next
 
-# COPY --from=builder /app/public ./public
-COPY --from=builder --chown=nextjs:nodejs /app/.next/standalone ./
-COPY --from=builder --chown=nextjs:nodejs /app/.next/static ./.next/static
+COPY --from=deps --chown=nextjs:nodejs /app/node_modules ./node_modules
+COPY --chown=nextjs:nodejs . .
+
+ENV NEXT_TELEMETRY_DISABLED=1
+EXPOSE 3000
+ENV PORT=3000
+ENV HOSTNAME="0.0.0.0"
 
 USER nextjs
-CMD ["node", "server.js"]
+CMD ["yarn", "dev"]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { socket } from "@/socket";
 import { Vote } from "@/utils/game";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import Confetti from "react-confetti";
 
 export default function Home() {
   const [name, setName] = useState("");
@@ -12,10 +13,11 @@ export default function Home() {
     process.env.NEXT_PUBLIC_DEFAULT_ISSUE_KEY ?? ""
   );
   const [playersCount, setPlayersCount] = useState(0);
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<Vote[]>([]);
   const resourcesLinks = JSON.parse(
     process.env.NEXT_PUBLIC_RESOURCES_LINKS ?? "{}"
   );
+  const [isExploding, setIsExploding] = useState(false);
 
   useEffect(() => {
     socket.on("newStory", (data) => {
@@ -24,15 +26,21 @@ export default function Home() {
       setResults([]);
     });
 
-    socket.on("revealVotes", (data) => {
+    socket.on("revealVotes", (data: Vote[]) => {
       console.info("Revealing", data);
       setResults(data);
+      if (data.every((vote) => vote.vote == data[0].vote)) {
+        // we all have the same vote
+        setIsExploding(true);
+      }
     });
 
     socket.on("updatePlayers", (data) => {
       console.info("New players joined", data);
       setPlayersCount(data);
     });
+
+    setName(localStorage.getItem("plap_name") ?? "");
 
     return () => {
       socket.off("newStory");
@@ -41,14 +49,24 @@ export default function Home() {
 
   return (
     <>
+      {isExploding ? (
+        <Confetti
+          recycle={false}
+          numberOfPieces={500}
+          onConfettiComplete={() => setIsExploding(false)}
+        />
+      ) : null}
+
       <div className="m-4">
-        {/* TODO make component and r/w from localstorage */}
         Name
         <br />
         <input
           type="text"
           value={name}
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => {
+            setName(e.target.value);
+            localStorage.setItem("plap_name", e.target.value);
+          }}
           placeholder="Best developer ever"
           className="bg-white text-black"
         />
@@ -67,15 +85,15 @@ export default function Home() {
         />
       </div>
 
-      <div>Players: {playersCount}</div>
+      <div className="m-4">Players: {playersCount}</div>
 
       <div className="m-4">
         <div>Your vote</div>
-        <VoteSelector name={name} />
+        <VoteSelector name={name} enabled={results.length == 0} />
       </div>
 
       {results.length > 0 ? (
-        <div className="m-4 border border-amber-600">
+        <div className="m-4 p-2 border border-amber-600">
           <div>Results</div>
           {results.map((result: Vote) => (
             <div key={result.name}>
